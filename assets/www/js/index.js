f734ddf7bfafa0f0a782a00e34f3a84de502c7e1
Martin Hommer <martin.hommer@netlight.com>
2013-06-15 10:02:23 +0200

    added entire android project// web code is in assets/www

---
+++ assets/www/js/index.js
@@ -0,0 +1,167 @@
+$(document).ready(function() {
+    initCommandes();
+	/* startApp after device ready */
+	document.addEventListener("deviceready", startApp, false);
+});
+
+
+/**
+ * Start the App
+ */
+function startApp() {
+	var oAuth = liquid.helper.oauth;
+	
+    if (oAuth.isAuthorized()) {
+    	console.log("oauth token already there.");
+        alert("oauth there");
+    } else {
+        alert("oauth not there");
+        liquid.helper.oauth.authorize(authorizeWindowChange);
+        event.preventDefault();
+    }
+}
+
+/*function getAccessToken() {
+    liquid.helper.oauth.getAccessToken(function(tokenObj) {
+        var token = tokenObj.access_token); 
+    });
+}*/
+
+
+function startPageTaskList() {
+     //alert('WORKS');
+    //$.mobile.changePage("#page-tasklist", {
+    //    transition : "none",
+    //});
+}
+
+
+
+function authorizeWindowChange(uriLocation) {
+    //console.log("Location Changed: " + uriLocation); 
+	var oAuth = liquid.helper.oauth;
+	
+	// oAuth process is successful!	
+    if (oAuth.requestStatus == oAuth.status.SUCCESS) {
+        var authCode = oAuth.authCode;
+
+        // have the authCode, now save the refreshToken and start Page TaskList
+        oAuth.saveRefreshToken({ 
+        	  	auth_code: oAuth.authCode
+        	  }, function() {
+        		  startPageTaskList();
+        	  });
+        
+    } 
+    else if (oAuth.requestStatus == oAuth.status.ERROR) 
+    {
+    	console.log("ERROR - status received = oAuth.status.ERROR");
+    } 
+    else {
+        // do nothing, since user can be visiting different urls
+    }
+}
+
+
+/**
+ * Populates the list of Tasks
+ */
+function populateTaskList() {
+	$.mobile.showPageLoadingMsg("a", "Loading Tasks...");
+	
+	/* hide all the request Info blocks/divs */
+	$('.request-info').hide();
+	
+	liquid.model.tasks.getList(function(data) {
+        $('#qt-listview-tasks').empty();
+        
+        console.log(JSON.stringify(data)); // debug JSON response data
+        
+        /* check if there's an error from server, then display
+         * error and retry
+         */
+        if (data.error) {
+        	console.log('Unable to load Task List >> ' + data.error.message);
+        	$.mobile.hidePageLoadingMsg();   
+            return;        	
+        }
+        
+        /* if there are no elements in it, then
+         * display the info message, and return */
+        if (!data.items) {
+        	$('#qt-listview-info').show();
+            $.mobile.hidePageLoadingMsg();        	
+            return;
+        }
+        
+        
+        for (var i = 0; i < data.items.length; i++) {
+        	
+        	var item = data.items[i];
+        	
+        	$('#qt-listview-tasks')
+        		.append('<li><h5>' 
+        					+ item.title +
+        				'</h5></li>');
+        }
+        
+        $('#qt-listview-tasks').listview('refresh');
+        $.mobile.hidePageLoadingMsg();   
+	});
+}
+
+
+function goHome() {
+	
+    $.mobile.changePage("#page-unauthorized", {
+        transition : "none",
+        reverse: false,
+        changeHash: false
+    });
+    
+}
+
+
+
+(function() {
+
+	$('#page-tasklist').live('pageshow', function(event) {
+		
+		if (!liquid.helper.oauth.isAuthorized()) {
+			goHome();
+			return;
+		}
+		
+		$('#btn-refresh').click(function(event) {
+			populateTaskList();
+			event.preventDefault();
+		});
+		
+		$('#btn-hide-error').click(function(event) {
+			
+			$('#qt-listview-error').hide();			
+			event.preventDefault();
+		});
+		
+		/* populateTaskList on page init */
+		populateTaskList();
+	
+		/**
+		 * Add the listeners for each of the components
+		 * 	Listeners are for:
+		 * - Title bar refresh btn (#head-menu-refresh)
+		 */
+		$('#head-menu-refresh').click(function(event) {
+		    populateTaskList();
+		    event.preventDefault();
+		});
+		
+		
+		$('#head-menu-signout').click(function(event) {
+		    liquid.helper.oauth.unAuthorize();
+		    goHome();
+		    event.preventDefault();
+		});
+	
+	});
+})();
---
+++ assets/www/js/index_old.js
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+//wrong: var clientId = '399944064628-3ie91ujnnklvfeeccco88j8rul89rcn9.apps.googleusercontent.com';
+var clientId = '399944064628-onrska27ej2hbggu3a2357gn6pbkc0pe.apps.googleusercontent.com';
+var clientSecret = '2p7Fcv3fAgqAwaCakaINS7NZ';
+var redirectURI = 'http://localhost';
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        getToken();
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:none;');
+
+        console.log('Received Event: ' + id);
+    }   
+};
+
+function getToken ()  {
+    var tokenCapture = function (url) {
+
+        var url = 'https://accounts.google.com/o/oauth2/auth?' +
+            'client_id=' + clientId + '&' +
+            'redirect_uri=' + redirectURI + '&' +
+            'scope=https://www.googleapis.com/auth/fusiontables&' +
+            'response_type=token&' +
+            'approval_prompt=force&' +
+            'login_hint=buzzlight.netlight@gmail.com';
+
+        this.document.location.href = url;
+    };
+
+    var url = this.window.location.href;
+    var tokenIndex = url.indexOf('access_token');
+
+    document.getElementById("token").innerHTML="bla";
+
+    if (tokenIndex >= 0) {
+        var token,
+            expirysecs,
+            type;
+
+        var firstAndIndex = url.indexOf('&');
+
+        token = url.substring(tokenIndex, firstAndIndex);
+        token = token.substr(token.indexOf('=')+1, token.length);
+
+        var expiryIndex = url.indexOf('expires_in');
+        expirysecs = url.substr(expiryIndex, url.length);
+        expirysecs = expirysecs.substr(expirysecs.indexOf('=')+1, expirysecs.length);
+
+        var tokenobject = {
+            token : token,
+            expiry : expirysecs * 1000,
+            timestamp : new Date().getTime()
+        }
+
+        writeToFile(tokenobject.toString(), 'logintoken');
+
+        readJSONFromFile('logintoken', function (text) {
+
+        });
+
+    } else {
+
+        tokenCapture();
+    }
+
+}
+
+/**
+ *
+ * @param object    JSON object to write
+ * @param filename  filename to write it to
+ */
+function writeToFile (object, filename) {
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile(filename, {create: true}, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+
+    function gotFileWriter(writer) {
+        writer.onwrite = function(evt) {
+            console.log("write success to file "+ filename);
+        };
+
+        writer.write(object);
+
+    }
+
+    function fail(error) {
+        console.log('error writing to file '+ filename + ". " + error.code);
+    }
+
+}
+
+
+function readJSONFromFile (filename, callback) {
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile(filename, null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file){
+        readAsText(file);
+    }
+
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function(evt) {
+            console.log("Read as text");
+            console.log(evt.target.result);
+            callback(evt.target.result);
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(evt) {
+        console.log(evt.target.error.code);
+    }
+}
+
+app.initialize();
+
